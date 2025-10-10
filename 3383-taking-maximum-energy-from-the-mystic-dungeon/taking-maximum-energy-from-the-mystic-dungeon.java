c2b3cb5966167e694d3312a1094ad43a6e5c67b9
Sarthak <98268602+Sarthakgiri455@users.noreply.github.com>
2025-10-11 00:17:27 +0530

    Time: 10 ms (10.34%) | Memory: 61 MB (63.45%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.java
@@ -1,27 +1,15 @@
 class Solution {
     public int maximumEnergy(int[] energy, int k) {
-        int[][]dp = new int[energy.length][2];
-        for(int[]row: dp) {
-            Arrays.fill(row, -1);
+        int[]kDistanceSubsequenceSum = new int[energy.length];
+        for(int i = energy.length-1;i>=0;i--) {
+            if((i+k)>=energy.length){
+                kDistanceSubsequenceSum[i] = energy[i];
+            }
+            else{
+                kDistanceSubsequenceSum[i] = energy[i] + kDistanceSubsequenceSum[i+k];
+            }
         }
-        return recursion(dp, 0, 0, k, energy, 0);
-    }
-    private int recursion(int[][]dp, int index, int choice, int k, int[]energy, int count) {
-        if(index>=dp.length && count ==0) {
-            return Integer.MIN_VALUE;
-        }
-        if(index>=dp.length){
-            return 0;
-        }
-        if(dp[index][choice]!= -1) 
-        {
-            return dp[index][choice];
-        }
-        int ans = energy[index] + recursion(dp, index+k, 1, k, energy, count+1);
-        if(choice==0){
-            ans = Math.max(ans, recursion(dp, index+1, choice, k, energy, count));
-        }
-        return dp[index][choice] = ans;
+        return Arrays.stream(kDistanceSubsequenceSum).max().getAsInt();
     }
 
 }
