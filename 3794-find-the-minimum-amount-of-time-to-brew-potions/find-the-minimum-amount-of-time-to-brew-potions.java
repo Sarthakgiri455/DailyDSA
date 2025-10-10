afdf2895ddc74f003e2bd9653c60b3aca4e86495
Sarthak <98268602+Sarthakgiri455@users.noreply.github.com>
2025-10-10 23:26:39 +0530

    Time: 149 ms (41.67%) | Memory: 44.5 MB (87.50%) - LeetSync

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
@@ -1,17 +1,17 @@
 class Solution {
     public long minTime(int[] skill, int[] mana) {
-        long[][]dp = new long[mana.length+1][skill.length+1];
+        long[]dp = new long[skill.length+1];
         
         for(int i=0; i < mana.length;i++) {
             for(int j =0;j<skill.length;j++) {
                 long currentTime = (long)skill[j]*mana[i];
-                dp[i+1][j+1] = Math.max(dp[i+1-1][j+1], dp[i+1][j-1+1]) + currentTime;
+                dp[j+1] = Math.max(dp[j-1+1], dp[j+1]) + currentTime;
             }
-            for(int j = dp[0].length-2; j>=1; j--){
-                dp[i+1][j] = dp[i+1][j+1] - (long)skill[j]*mana[i];
+            for(int j = dp.length-2; j>=1; j--){
+                dp[j] = dp[j+1] - (long)skill[j]*mana[i];
             }
         }
-        return dp[mana.length][skill.length];
+        return dp[skill.length];
         // ith potion -> jth person
         // jth person ->i-1 potion
         // ith potion -> j-1th person
