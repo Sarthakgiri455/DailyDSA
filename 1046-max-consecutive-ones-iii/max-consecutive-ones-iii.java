66dfeadbd660b354f4d23a6056431da16c374161
Sarthak <98268602+Sarthakgiri455@users.noreply.github.com>
2025-10-22 18:56:58 +0530

    Time: 1104 ms (5.00%) | Memory: 53.9 MB (5.25%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -3,18 +3,20 @@ class Solution {
         int remaining = k;
         int max = 0;
         int left =0;
+        TreeSet<Integer>zeroIndices = new TreeSet<>();
+
         for(int right = 0; right<nums.length;right++) {
-            if(nums[right]==0) {
+            
+            if(nums[right] == 0){
                 remaining-=1;
+                zeroIndices.add(right);
             }
-            while(remaining<0) {
-                if(nums[left]==0){
-                    left+=1;
-                    remaining+=1;
-                    break;
-                }  
-                left+=1;          
+            if(remaining<0){
+                int val = zeroIndices.removeFirst();
+                left = val+1;
+                remaining+=1;
             }
+            
             
             max = Math.max(max, right-left+1);
             System.out.println("Left and right are " + left + " " + right);
