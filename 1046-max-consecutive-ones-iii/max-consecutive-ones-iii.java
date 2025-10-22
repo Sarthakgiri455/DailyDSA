156e960b04f7e700b4c5ab26bdb6374fc4c4f0d6
Sarthak <98268602+Sarthakgiri455@users.noreply.github.com>
2025-10-22 19:19:39 +0530

    Time: 4 ms (38.16%) | Memory: 53 MB (56.85%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,27 +1,20 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int remaining = k;
+        int left = 0;
+        int count = 0;
         int max = 0;
-        int left =0;
-        TreeSet<Integer>zeroIndices = new TreeSet<>();
-
-        for(int right = 0; right<nums.length;right++) {
-            
-            if(nums[right] == 0){
-                remaining-=1;
-                zeroIndices.add(right);
+       for(int right = 0;right<nums.length;right++) {
+            if(nums[right]==0){
+                count+=1;
             }
-            if(remaining<0){
-                int val = zeroIndices.removeFirst();
-                left = val+1;
-                remaining+=1;
+            if(count>k){
+                if(nums[left]==0){
+                    count-=1;
+                }
+                left+=1;
             }
-            
-            
             max = Math.max(max, right-left+1);
-            System.out.println("Left and right are " + left + " " + right);
-
-        }
-        return max;
+       }
+       return max;
     }
 }
